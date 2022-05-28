5d1f5ac9bc9404d168b5299ebc105ca49a04f448
bensandison <sandisonben@gmail.com>
2022-05-29 00:54:02 +0100

    clicking changes color

--- src/scenes/ColorTaurus.js
+++ src/scenes/ColorTaurus.js
@@ -13,30 +13,26 @@ export default function ColorTaurus({ mouseRef }) {
 
 	useEffect(() => {
 		if (!mouse.isDown) return;
-		else boxRef.current.color = "red";
+
+		// jump through colorArray
+		setIndex((prev) => {
+			if (prev > colorArray.length - 2) return 0;
+			else return prev + 1;
+		});
 	}, [mouse.isDown, mouse.isTouch]);
 
 	const boxRef = useRef();
+
 	useFrame((state, delta) => {
 		boxRef.current.rotation.x += 0.02;
 		boxRef.current.rotation.y += 0.02;
-	});
-
-	const [wireframe, setWireframe] = useState(false);
-
-	useEffect(() => {
-		if (mouse.isDown) setWireframe(true);
-		else setWireframe(false);
 	});
 
 	return (
 		<>
 			<mesh ref={boxRef} scale={[1, 1, 1]}>
 				<torusKnotGeometry args={[1.5, 0.5]}></torusKnotGeometry>
-				<meshNormalMaterial
-					wireframe={wireframe}
-					// flatShading
-				></meshNormalMaterial>
+				<meshBasicMaterial color={colorArray[index]}></meshBasicMaterial>
 			</mesh>
 		</>
 	);
